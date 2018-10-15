c8b52fc23fed62c8ccf379bb867063b8731b6a80
aeadigt <aea@digt.ru>
2018-10-15 13:10:26 +0000

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -62,8 +62,8 @@ fi
 
 cd $(dirname $0)
 
-wget -nc https://pjreddie.com/media/files/yolov3.weights
-wget -nc https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg
+wget -nc https://pjreddie.com/media/files/yolov3-tiny.weights
+wget -nc https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3-tiny.cfg
 wget -nc https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names
 
 wget -nc https://github.com/pjreddie/darknet/raw/master/data/dog.jpg
